900611db04c2cb09f24f84fecb221072f15b70d3
JohnRowleyDorsetCollege <john.rowley@dorset.ie>
2024-11-18 10:00:14 +0000

    Added in booking controller

--- UtopiaTours.API/Program.cs
+++ UtopiaTours.API/Program.cs
@@ -1,6 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using UtopiaTours.API;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+string connectionString = "Server=localhost;Database=UtopiaTours;User=root;Password=root";
+
+//builder.Services.AddDbContext<UtopiaToursContext>(options => options.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 32))));
+builder.Services.AddDbContext<UtopiaToursContext>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
--- UtopiaTours.API/UtopiaToursContext.cs
+++ UtopiaTours.API/UtopiaToursContext.cs
@@ -9,8 +9,20 @@ namespace UtopiaTours.API
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            string connectionString = "Server=localhost;Database=Utopia;User=root;Password=root";
+
+           
+            string connectionString = "Server=localhost;Database=UtopiaTours;User=root;Password=root";
             optionsBuilder.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 32)));
+        
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+           foreach(var entityType in modelBuilder.Model.GetEntityTypes())
+            {
+                entityType.SetTableName(entityType.DisplayName());
+            }
+          //  base.OnModelCreating(modelBuilder); 
         }
     }
 }
