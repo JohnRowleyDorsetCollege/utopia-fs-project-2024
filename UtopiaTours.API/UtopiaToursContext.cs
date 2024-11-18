3d97e49f938e2cfabbcdbd18ee4631e83b7cca55
JohnRowleyDorsetCollege <john.rowley@dorset.ie>
2024-11-18 09:42:17 +0000

    Added mysql connector

---
+++ UtopiaTours.API/UtopiaToursContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UtopiaTours.Domain;
+
+namespace UtopiaTours.API
+{
+    public class UtopiaToursContext : DbContext
+    {
+        public DbSet<Booking> Bookings { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = "Server=localhost;Database=Utopia;User=root;Password=root";
+            optionsBuilder.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 32)));
+        }
+    }
+}
---
+++ UtopiaTours.Domain/Entities.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtopiaTours.Domain
+{
+    public class Booking
+    {
+        [Key]
+        public int Id { get; set; }
+        public int PassengerId { get; set; }
+        public int ScheduleId { get; set; }
+        public DateTime BookingDt { get; set; }
+    }
+}
