84c600e44d1313613e2c32c97c58f7080304088f
JohnRowleyDorsetCollege <john.rowley@dorset.ie>
2024-11-18 09:31:55 +0000

    Add project files.

---
+++ UtopiaTours.Domain/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace UtopiaTours.Domain
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ UtopiaTours.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace UtopiaTours.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
